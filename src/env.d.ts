ed0974363f1b878bf58030d4c114690dbc91dfb4
Guavka <iluminat94@gmail.com>
2022-08-17 13:33:52 +0300

    add vite plugins

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,14 @@
-/* eslint-disable */
+/// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string
+  readonly VITE_TITLE: string
+  readonly VITE_SHORT_TITLE: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: string;
